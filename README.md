18bcf71a9cd3ce38631e447187b208468f9e298a
ChadHartman <chad+github@tealium.com>
2015-11-10 11:20:00 -0800

    Added sample projects and aar

---
+++ README.md
@@ -0,0 +1,7 @@
+## Brief
+## What is Tealium?
+## Hot to get started
+## Contact us
+## Change Log
+## License
+Copyright (c) Tealium 2015
---
+++ Samples/BlankApp+Tealium/app/src/main/java/com/tealium/blankapp/helper/TealiumHelper.java
@@ -0,0 +1,38 @@
+package com.tealium.blankapp.helper;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Application;
+import android.os.Build;
+import android.webkit.WebView;
+
+import com.tealium.blankapp.BuildConfig;
+import com.tealium.library.Tealium;
+
+import java.util.Map;
+
+public final class TealiumHelper {
+
+    public static final String TEALIUM_MAIN = "main";
+
+    @SuppressLint("NewApi")
+    public static void initialize(Application application) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && BuildConfig.DEBUG) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+
+        Tealium.Config config = Tealium.Config.create(application, "tealiummobile", "demo", "dev");
+        Tealium.createInstance(TEALIUM_MAIN, config);
+    }
+
+    public static void onResume(Activity activity, Map<String, String> data) {
+        Tealium.getInstance(TEALIUM_MAIN)
+                .trackView(activity.getClass().getSimpleName(), data);
+    }
+
+    public static void trackEvent(String eventName, Map<String, String> data) {
+        Tealium.getInstance(TEALIUM_MAIN)
+                .trackEvent(eventName, data);
+    }
+}
