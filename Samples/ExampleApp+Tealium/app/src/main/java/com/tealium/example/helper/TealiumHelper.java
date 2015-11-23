bd810904fd7b687f89eed5cf309aa3c39b56938c
ChadHartman <chad+github@tealium.com>
2015-11-23 11:27:31 -0800

    updated jar with fixed mps parsing

--- Samples/ExampleApp+Tealium/app/src/main/java/com/tealium/example/helper/TealiumHelper.java
+++ Samples/ExampleApp+Tealium/app/src/main/java/com/tealium/example/helper/TealiumHelper.java
@@ -43,7 +43,7 @@ public final class TealiumHelper {
 
         Tealium.Config config = Tealium.Config.create(application, "tealiummobile", "demo", "dev");
 
-        config.setOverridePublishURL("https://tags.tiqcdn.com/qa6/tealiummobile/ios/dev/mobile.html");
+        config.setOverridePublishURL("https://tags.tiqcdn.com/qa6/tealiummobile/android/dev/mobile.html");
 
         // Get the WebView with UTag loaded
         config.getEventListeners().add(new WebViewLoadListener() {
